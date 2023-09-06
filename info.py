5acf6c74ac9c65e85f8aeaf47e267361803c1a53
SAAMNETWORK <140245198+SAAMNETWORK@users.noreply.github.com>
2023-09-06 14:31:49 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,7 +43,7 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://realhulk6:realhulk@cluster0.ryimidk.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://AhmeAlisha:karishama@cluster0.hpqm6tc.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "mongo")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
